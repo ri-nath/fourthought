772018eacffcda90a6d383d38bb0ce23d7bf0f11
Rishi Nath <rishi.nath@gmail.com>
2019-09-01 20:32:36 -0700

    rough 3 layer no negatives/prediction algorithm

--- src/com/vayelcrau/fourthought/Fourthought.java
+++ src/com/vayelcrau/fourthought/Fourthought.java
@@ -1,5 +1,6 @@
 package com.vayelcrau.fourthought;
 
+import com.vayelcrau.fourthought.game.Board;
 import com.vayelcrau.fourthought.game.Game;
 import lib.Values;
 
@@ -9,6 +10,7 @@ public class Fourthought{
     private Game game;
     private Random rand;
     private final Values color = Values.RED;
+    private int depth;
 
     private static Fourthought instance;
 
@@ -23,6 +25,44 @@ public class Fourthought{
     }
 
     public void move() {
-        game.place(color, rand.nextInt(7));
+        depth = 0;
+        Board currentBoard = game.getBoard().getCopyOfBoard();
+        game.place(color, calculateBestMove(currentBoard));
+    }
+
+    public int calculateBestMove(Board currentBoard) {
+        int bestMove = testAllMoves(currentBoard);
+
+        if (bestMove != -1) return bestMove;
+
+        for (int i = 0; i < 7; i++) {
+            Board board = currentBoard.getCopyOfBoard();
+            board.place(color, i);
+            bestMove = testAllMoves(board);
+            if (bestMove != -1) return bestMove;
+        }
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                Board board = currentBoard.getCopyOfBoard();
+                board.place(color, i);
+                board.place(color, j);
+                bestMove = testAllMoves(board);
+                if (bestMove != -1) return bestMove;
+            }
+        }
+
+        return rand.nextInt(7);
+
+    }
+
+    public int testAllMoves(Board board) {
+        for (int i = 0; i < 7; i++) {
+            if (board.testMove(color, i)) {
+                System.out.println("Found winning move at "+ i+ "!");
+                return i;
+            }
+        }
+        return -1;
     }
 }
