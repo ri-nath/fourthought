c3f7a126426380aace91b14d91a60c2a247e388f
Rishi Nath <rishi.nath@gmail.com>
2019-09-04 21:20:54 -0700

    woops

--- src/com/vayelcrau/fourthought/Constants.java
+++ src/com/vayelcrau/fourthought/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static final Values PLAYER_COLOR = Values.BLUE;
 
     public static final int MAX_LAYERS = 8;
-    public static final int MIN_LAYERS = 1;
+    public static final int MIN_LAYERS = 2;
     public static final int DEFAULT_LAYERS = 6;
 }
